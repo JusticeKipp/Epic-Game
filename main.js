810e390afa087f28b3198ab7e970fe340672116f
NathanH2111 <nathan@inhouseprinting.net>
2021-12-01 15:10:00 -0500

    added audio and edited some js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Scooter'; /* The text */
+var txt = ''; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function createListItem() {
@@ -7,12 +7,22 @@ function createListItem() {
    node.appendChild(document.createTextNode('Scooter'));
    document.querySelector('ul').appendChild(node);
    txt = node.innerHTML;
-}
-
-function typeWriter() {
    if (i < txt.length) {
       document.getElementById("text").innerHTML += txt.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
    }
+}
+
+// function typeWriter() {
+// }
+let introSound = new Audio("voicebooking-speech.mp3");
+let gameMusic = new Audio("Game-Menu_Looping.mp3")
+gameMusic.loop = true;
+function Sound() {
+   introSound.play();
+   gameMusic.play();
+   document.getElementById("startingPage").style.display = "none";
+   document.getElementById("PlayBox").style.display = "flex";
+   document.getElementById("StoryBox").style.display = "flex";
 }
