e2f5a2cf43a22a9d90b06647d0692cc17d7c508d
NathanH2111 <nathan@inhouseprinting.net>
2021-12-01 18:38:50 -0500

    some js and css changes

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 var i = 0;
-var txt = ''; /* The text */
+let txt = ''; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function createListItem() {
+   // if (document.querySelector('div').removeAttribute('id') != null) {
+   // document.querySelector('li').removeAttribute('id');
+   // }
    let node = document.createElement('li');
-   node.appendChild(document.createTextNode('Scooter'));
+   node.setAttribute('id', "text")
+   node.appendChild(document.createTextNode(''));
    document.querySelector('ul').appendChild(node);
-   txt = node.innerHTML;
-   if (i < txt.length) {
-      document.getElementById("text").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
+   txt = "pizza";
+
+   function typeWriter() {
+      if (i < txt.length) {
+         document.getElementById("text").innerHTML += txt.charAt(i);
+         i++;
+         setTimeout(typeWriter, speed);
+      }
    }
+   typeWriter();
 }
 
 // function typeWriter() {
@@ -20,8 +28,8 @@ let introSound = new Audio("voicebooking-speech.mp3");
 let gameMusic = new Audio("Game-Menu_Looping.mp3")
 gameMusic.loop = true;
 function Sound() {
-   introSound.play();
-   gameMusic.play();
+   // introSound.play();
+   // gameMusic.play();
    document.getElementById("startingPage").style.display = "none";
    document.getElementById("PlayBox").style.display = "flex";
    document.getElementById("StoryBox").style.display = "flex";
