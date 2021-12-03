6805a1af85b2ca248f58767434dd17eb2db73e19
NathanH2111 <nathan@inhouseprinting.net>
2021-12-02 22:39:49 -0500

    I changed a lot

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var i = 0;
-let txt = ''; /* The text */
+let txt = '';
 var speed = 50; /* The speed/duration of the effect in milliseconds */
+let paused = false;
 
 function createListItem() {
    //checks if the id text exists and removes it 
@@ -12,25 +13,28 @@ function createListItem() {
    //gives new list item the id of text
    node.setAttribute('id', "text")
    //defines variable txt as 
-   txt = "pizza";
+   txt = "pi";
    //adds the list item to the ul tag
-   // node.appendChild(document.createTextNode(txt));
    document.querySelector('ul').appendChild(node);
 
    i = 0;
 
    function typeWriter() {
+      document.getElementById("fight").style.display = "none";
+      document.getElementById("move").style.display = "none";
       if (i < txt.length) {
          document.getElementById("text").innerHTML += txt.charAt(i);
          i++;
          setTimeout(typeWriter, speed);
       }
+      if (i == txt.length) {
+         document.getElementById("fight").style.display = "block";
+         document.getElementById("move").style.display = "block";
+      }
    }
    typeWriter();
 }
 
-// function typeWriter() {
-// }
 let introSound = new Audio("voicebooking-speech.mp3");
 let gameMusic = new Audio("Guitar-Gentle.mp3");
 gameMusic.loop = true;
@@ -39,10 +43,25 @@ function Sound() {
    gameMusic.play();
    document.getElementById("startingPage").style.display = "none";
    document.getElementById("page2").style.display = "flex";
+   document.getElementById("sound").style.display = "inline";
 }
 
 function Intro() {
    document.getElementById("page2").style.display = "none";
    document.getElementById("StoryBox").style.display = "flex";
    document.getElementById("PlayBox").style.display = "flex";
+}
+
+//Mute button function 
+function muteMusic() {
+   if (paused == true) {
+      gameMusic.play();
+      document.getElementById("soundButton").innerHTML = "<ion-icon title='Mute' name='volume-high-outline'></ion-icon>"
+      paused = false
+   }
+   else {
+      gameMusic.pause();
+      document.getElementById("soundButton").innerHTML = '<ion-icon title="Unmute" name="volume-mute-outline"></ion-icon>'
+      paused = true
+   }
 }
